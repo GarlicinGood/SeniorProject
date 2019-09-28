219569593be62bfe05d87ea0f2887fceff1c4d0f
Nick Negomir <NickN@Nicks-MacBook-Air.local>
2019-09-28 15:47:13 -0600

    Tomato

--- dist/config.js
+++ dist/config.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = {
-    "token": "NjIzMzAxNDg1OTAzNjA5ODU3.XYAg6Q.VF2NPSESqIY84whWa0BPcRtzER4",
+    "token": "NjIzMzAxNDg1OTAzNjA5ODU3.XY_UZg.Vpt3Qti5sUE4kV6oNx5NMrwS6H0",
     "prefix": ".",
     "commands": [
         "testCommand",
