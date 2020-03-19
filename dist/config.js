3b895e8a5ab28324c121800c81af107f0fb51c2a
GarlicinGood <55930556+GarlicinGood@users.noreply.github.com>
2020-03-19 14:23:16 -0600

    Add files via upload

--- dist/config.js
+++ dist/config.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = {
-    "token": "NjQyMTcxNTI5ODQ0NjIxMzc5.XcTNlg.6nEAVks7SW7QV_T6pxjw2V5ojPc",
-    "prefix": ".",
+    "token": "NjIzMzAxNDg1OTAzNjA5ODU3.XipJ0w.QXdekOOpZhrURfDNqywHhB0DLAc",
+    "prefix": "/",
     "commands": [
         "testCommand",
         "kick",
         "supportTicket"
     ]
 };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFXLFFBQUEsTUFBTSxHQUFHO0lBQ2hCLE9BQU8sRUFBRSw2REFBNkQ7SUFDdEUsUUFBUSxFQUFFLEdBQUc7SUFDYixVQUFVLEVBQUU7UUFDUixhQUFhO1FBQ2IsTUFBTTtRQUNOLGVBQWU7S0FFbEI7Q0FDSixDQUFBIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFXLFFBQUEsTUFBTSxHQUFHO0lBQ2hCLE9BQU8sRUFBRSw2REFBNkQ7SUFDdEUsUUFBUSxFQUFFLEdBQUc7SUFDYixVQUFVLEVBQUU7UUFDUixhQUFhO1FBQ2IsTUFBTTtRQUNOLGVBQWU7S0FRbEI7Q0FDSixDQUFBIn0=
