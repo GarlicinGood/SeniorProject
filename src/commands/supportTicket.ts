1a205b7ae704c1ff26dcc6eec4f70f78293055e8
GarlicinGood <55930556+GarlicinGood@users.noreply.github.com>
2020-03-19 14:23:56 -0600

    Add files via upload

--- src/commands/supportTicket.ts
+++ src/commands/supportTicket.ts
@@ -1,5 +1,6 @@
 import * as Discord from "discord.js";
 import { IBotCommand } from "../api";
+import { create } from "domain";
 
 export default class testCommand implements IBotCommand {
 
@@ -7,35 +8,77 @@ export default class testCommand implements IBotCommand {
 
     help(): string {
         return "Creates a private text channel for help.";
-    }    
-    
+    }
+
     isThisCommand(command: string): boolean {
-        
+
         return command === this._command;
     }
-    
+
     runCommand(args: string[], messageObject: Discord.Message, client: Discord.Client): void {
-        
-        //Make a new text channel
-        let guid = Date.now().toString();
-        // messageObject.guild.createChannel(`Test ChanneL: ${guid}`, { type: 'text'});
-        
         let guild = messageObject.guild;
         let currentUser = client.user;
 
-        var name = `support-ticket-${guid}`;
-        guild.createChannel(name, 'text', [
-            {
-                id: messageObject.author.id,
-                allow: ['VIEW_CHANNEL', 'SEND_TTS_MESSAGES'],
-            },
-        ]);
+        // let botLogs = Discord.Channel.name === "bot-logs"
+        // const results = messageObject.channel.fetchMessage(`${botLogs}`)
 
-        //make sure everyone can't see this channel
-        //add person who made this channel
+        // var logsChannel:Discord.TextChannel = guild.channels.find();
+
+
+        //GuildChannel
+        var logsChannel = guild.channels.find(channel => channel.id == "bot-logs");
+        var whatever = messageObject.channel;
+
+        whatever.fetchMessage("672635523419865098")
+            .then(message => {
+                console.log("I found da message")
+                
+                var id:number = Number(message.content)
+                var name = `support-ticket-${id.toString()}`;
+                guild.createChannel(
+                    name,
+                    {
+                        type: 'text',
+                        permissionOverwrites: [
+                            { id: messageObject.author.id, allow: ['VIEW_CHANNEL', 'SEND_MESSAGES'] }
+                            // ,{ id: .author.id, deny: ['VIEW_CHANNEL']}
+                        ],
+                        parent: '642183412907507721'
+                    }
+                )
+
+                id = id + 1;
+                message.edit((id).toString())
+            })
+            .catch(error => console.log(error))
+            ;
+
+            
+
+
+        // let logChannel = Discord.Channel.name === "bot-logs"
+        // messageObject.channel.(`${logChannel}`)
+        //     .then(console.log)
+        //     .catch(console.error);
+
+        // TODO - Hide channel from everyone else, 
+        // parent into support catergory, 
+        //assure admins are allowed into text channel, 
+        //upload the number to an online database
+
+
+
+        //642183412907507721 (<==id of support category)
+
+
+
+
+
         //rick roll user who made this channel
-        
+
     }
+
+
 
 
 }
